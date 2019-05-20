c3ec377a66e2bf90f2e5fdde4c65c6021da1157f
ixhimanshu <himanshu011196@gmail.com>
2019-05-20 07:36:15 +0530

    pointing port changed to github

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // Make connection
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('https://ixhimanshu.github.io/chatApp/');
 
 // Query DOM
 var message = document.getElementById('message'),
